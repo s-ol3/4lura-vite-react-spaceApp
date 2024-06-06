ba5133a171b9b89464a82bff1b8f0511d175a5db
sol3 <marzen25@gmail.com>
2024-06-06 14:11:50 -0300

    hasta cabecera y barra lateral, reemplace lo mio

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-//import './App.css' ----------sacamos xq vamos a trabajar con styled components
-
-
 import styled from "styled-components"
 import GlobalStyles from "./components/GlobalStyles"
-
-
+import Cabecera from "./components/Cabecera"
+import BarraLateral from "./components/BarraLateral"
 const FondoGradiente = styled.div`
 background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
 width:100%;
@@ -22,37 +15,11 @@ function App() {
     <>
       <FondoGradiente>
         <GlobalStyles />
+        <Cabecera />
+        <BarraLateral />
       </FondoGradiente>
-
-
-
-
-
-      {/* <div>           ''vite + react''
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </>
   )
 }
 
 export default App
-
-
-
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-//import './index.css' -borrado por uso de styled components
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
