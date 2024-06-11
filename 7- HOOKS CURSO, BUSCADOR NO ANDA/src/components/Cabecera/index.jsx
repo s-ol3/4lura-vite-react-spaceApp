d3578ead361241c1861516dcf64f3a78f9d98569
sol3 <marzen25@gmail.com>
2024-06-11 12:04:20 -0300

    agrego curso 5 react hooks, trabajé sobre mismo repo

---
+++ 7- HOOKS CURSO, BUSCADOR NO ANDA/src/components/Cabecera/index.jsx
@@ -0,0 +1,44 @@
+/* import styled from "styled-components"
+import CampoTexto from "../CampoTexto"
+
+
+const HeaderEstilizado = styled.header`
+   padding: 60px 0;
+   display: flex;
+    justify-content: space-between;
+    img{
+        width: 212px;
+    }
+`
+
+const Cabecera = ({ setConsulta, textoConsulta }) => {      HOOKS 
+    return <HeaderEstilizado>
+        <img src="img/logo.png" alt="Logo de Space App" />
+        <CampoTexto setConsulta={setConsulta} textoConsulta={textoConsulta} />
+    </HeaderEstilizado>
+}
+
+export default Cabecera */
+
+import styled from "styled-components";
+import CampoTexto from "../CampoTexto";
+
+const HeaderEstilizado = styled.header`
+  padding: 60px 0;
+  display: flex;
+  justify-content: space-between;
+  img {
+    width: 212px;
+  }
+`;
+
+const Cabecera = ({ setConsulta, textoConsulta }) => {
+    return (
+        <HeaderEstilizado>
+            <img src="img/logo.png" alt="Logo de Space App" />
+            <CampoTexto setConsulta={setConsulta} textoConsulta={textoConsulta} />
+        </HeaderEstilizado>
+    );
+};
+
+export default Cabecera;
---
+++ src/components/Cargando/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const Cargando = () => {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+            {/* <img src="img/loading.gif" style={{ width: '30vw' }} /> */}
+
+
+            {/*  <img src="public/img/loading12.gif" style={{ width: '10vw', opacity: 0.2, margin: '60px' }} />  */} {/* 11 y 12 gif van */}
+
+
+
+
+            {/* <img src="public/img/loading9.gif" style={{ width: '20vw', margin: '60px' }} /> */}
+
+
+            {/*   <img src="public/img/loading18.gif" style={{ width: '10vw', opacity: 0.25, margin: '60px' }} /> */}  {/* 16 y 15 okok */}
+
+
+
+            {/* fontawesome era mejor    https://docs.fontawesome.com/v5/web/style/animate */}
+
+            <i class="fas fa-spinner fa-spin" style={{ display: 'block', margin: '60px', fontSize: '50px', color: '#c98cf1' }} ></i>
+
+            <i class="fas fa-spinner fa-pulse" style={{ display: 'block', margin: '60px', fontSize: '50px', color: '#c98cf1' }} ></i>
+            
+            <i class="fas fa-circle-notch fa-spin" style={{ display: 'block', margin: '60px', fontSize: '50px', color: '#c98cf1' }} ></i>
+
+
+        </div>
+    )
+}
+
+export default Cargando
